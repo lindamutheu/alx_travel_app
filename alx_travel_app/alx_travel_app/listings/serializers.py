6d3bc8b03f2835640d91cd572f450dd313bc9369
Alpha Infinos <kevinmullerhi@gmail.com>
2025-05-30 06:50:23 +0300

    milietone 2

---
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class ListingSerializer(serializers.ModelSerializer):
+    host = serializers.StringRelatedField(read_only=True)  # Or use a nested serializer if needed
+
+    class Meta:
+        model = Listing
+        fields = [
+            'property_id',
+            'host',
+            'name',
+            'description',
+            'location',
+            'price_per_night',
+            'created_at',
+            'updated_at',
+        ]
+class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    property = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Booking
+        fields = [
+            'booking_id',
+            'property',
+            'user',
+            'start_date',
+            'end_date',
+            'total_price',
+            'status',
+            'created_at',
+        ]
